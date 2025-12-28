7f614cef9a02b5d1894079efc54f3584b30180c1
ABHISHEK RAJ PATEL <technicalabhishekexpert@gmail.com>
2025-12-28 11:29:43 +0530

    Implement fraud detection prediction in predict.py

---
+++ predict.py
@@ -0,0 +1,13 @@
+import joblib
+
+model = joblib.load("fraud_model.pkl")
+
+# Sample order
+sample_order = [[2000, 0, 3, 2, 0.6]]  # COD, high risk
+
+prediction = model.predict(sample_order)
+
+if prediction[0] == 1:
+    print("🚨 Fraudulent Order Detected")
+else:
+    print("✅ Genuine Order")
